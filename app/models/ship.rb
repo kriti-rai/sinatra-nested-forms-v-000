3d7c495161520bc89b303dd3388cdb0abe99ef51
Kriti Rai <kriti.rai@gmail.com>
2018-05-20 20:08:41 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,8 +1,16 @@
 class Ship
   attr_accessor :name, :booty
 
+  SHIPS = []
+
   def initialize(args)
     @name = args[:name]
     @booty = args[:booty]
+    SHIPS << self
   end
+
+  def self.all
+    SHIPS
+  end
+  
 end
